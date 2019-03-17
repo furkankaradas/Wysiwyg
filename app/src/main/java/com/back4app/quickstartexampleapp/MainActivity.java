be27e45329317b5533d6f3ba82778df157954d99
Furkan Karadaş <furkankaradas3@gmail.com>
2019-03-17 21:13:11 +0300

    Inıtial Code

--- app/src/main/java/com/back4app/quickstartexampleapp/MainActivity.java
+++ app/src/main/java/com/back4app/quickstartexampleapp/MainActivity.java
@@ -1,40 +1,37 @@
 package com.back4app.quickstartexampleapp;
-import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private Button signInButton;
-    private Button logInButton;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        signInButton = findViewById(R.id.signInButton);
-        signInButton.setOnClickListener(this);
-        logInButton = findViewById(R.id.logInButton);
-        logInButton.setOnClickListener(this);
-
         // Save the current Installation to Back4App
+        ParseObject entity = new ParseObject("Human");
+
+        entity.put("name", "Furkan");
+
+        // Saves the new object.
+        // Notice that the SaveCallback is totally optional!
+        entity.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                // Here you can handle errors, if thrown. Otherwise, "e" should be null
+            }
+        });
+
+
         ParseInstallation.getCurrentInstallation().saveInBackground();
+
     }
 
-    @Override
-    public void onClick(View view) {
-        if(view == signInButton) {
-            Intent signInIntent = new Intent(this, activity_signIn.class);
-            startActivity(signInIntent);
-        }
-        if(view == logInButton) {
-            Intent signInIntent = new Intent(this, activity_logIn.class);
-            startActivity(signInIntent);
-        }
-    }
 }
